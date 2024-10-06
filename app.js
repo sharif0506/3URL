9f66104c17ffd7e295a3ae3df3fea452f7ded563
Md Sharifur Rahman <sharif.rahman0506@gmail.com>
2024-10-06 23:47:16 +0300

    .env file added, and dotenv package included

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import userRouter from './user/routes.js';
 import urlRouter from "./url/Route/UrlRoutes.js";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
 
@@ -15,12 +19,21 @@ app.use(cors());
 
 // MongoDB Connection
 try {
-    await mongoose.connect('mongodb://127.0.0.1/3url');
+    const mongoDbUrl = process.env.MONGODB_URL;
+
+    console.log(mongoDbUrl, typeof mongoDbUrl);
+
+    await mongoose.connect(mongoDbUrl);
 } catch (exception) {
     console.log(exception)
 }
 
 app.use('/api/users', userRouter);
 app.use('/api/urls', urlRouter);
+
+app.get('/:shortCode', (req, res) => {
+    // redirect to original url
+    res.redirect("http://www.youtube.com");
+});
 
 export default app;
